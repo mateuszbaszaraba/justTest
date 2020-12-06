3baf4dbf312b673da5a8fb33daccbe7d5252f773
mateuszbaszaraba <mateuszbaszaraba@outlook.com>
2020-12-06 12:27:31 +0100

    static

--- main.cpp
+++ main.cpp
@@ -2,34 +2,26 @@
 
 using namespace std;
 
-double poczatek_x;
-double poczatek_y;
-double skala_x = 1;
-double skala_y = 1;
-
-inline double wspx(double wspolrzedna)
-{
-    return((wspolrzedna - poczatek_x) * skala_x);
-}
-
-inline double wspy(double wspolrzedna)
-{
-    return((wspolrzedna - poczatek_y) * skala_y);
-}
-
+void red();
+void blue();
 int main() {
-    double x1 = 100, y1 = 100;
-    cout << "x = " << wspx(x1) << " y = " << wspy(y1) << endl;
-
-    poczatek_x = 20;
-    poczatek_y = -500;
-
-    cout << "Przesuniecie poczatku (x=20, y=-500)" << endl;
-    cout << "x = " << wspx(x1) << " y = " << wspy(y1) << endl;
-
-    cout << "Zmiana skali (x = 0.5)" << endl;
-    skala_x = 0.5;
-    cout << "x = " << wspx(x1) << " y = " << wspy(y1) << endl;
+    red();
+    blue();
+    red();
+    red();
+    blue();
 
     return 0;
+}
+
+void red() {
+    static int num;
+    ++num;
+    cout << "Red: " << num << endl;
+}
+
+void blue() {
+    static int num = 100;
+    cout << "Blue: " << num << endl;
+    ++num;
 }
