73ea69f021998511b304618aa27e8838e38d7c7c
mateuszbaszaraba <mateuszbaszaraba@outlook.com>
2020-12-09 20:49:48 +0100

    func return l-value

--- main.cpp
+++ main.cpp
@@ -2,25 +2,50 @@
 // Created by Mateusz Baszaraba on 08/12/2020.
 //
 #include <iostream>
-#include "nagl.h"
+#include <string>
 
 using namespace std;
 
-int ile_pigmejow = 9;
+int globalny_jedynkowy {25};
+int globalny_inny {77};
+
+int f_wartosc(int ktory)
+{
+    int lokalny {4};
+    if(ktory == 1) return globalny_jedynkowy;
+    else return globalny_inny;
+}
+
+int & f_ref_lwartosc(int ktory)
+{
+    int lokalny {4};
+    if(ktory == 1) return globalny_jedynkowy;
+    else return globalny_inny;
+}
+
+void wypisz(string txt, int a)
+{
+    cout << txt;
+    if(a) cout << a << ", ";
+    cout << "globalny_jedynkowy = " << globalny_jedynkowy << "globalny_inny = " << globalny_inny << endl;
+}
 
 int main() {
-    cout << "Początek programu" << endl;
-    funkcja_francuska();
-    funkcja_niemiecka();
-    cout << "Koniec programu" << endl;
+    int n {0};
+    n = f_wartosc(1);
+    wypisz("rezultat= ", n);
+    n = f_wartosc(6);
+    wypisz("rezultat= ", n);
 
+    n = f_ref_lwartosc(1);
+    wypisz("rezultat2= ", n);
+    n = f_ref_lwartosc(6);
+    wypisz("rezultat2= ", n);
+
+    f_ref_lwartosc(1) = 5000;
+    wypisz("wypisane", 0);
+
+    f_ref_lwartosc(6) = 7500;
+    wypisz("wypisane", 0);
     return 0;
-}
-
-void funkcja_etiopska() {
-    cout << " etipska ";
-}
-
-void funkcja_kenijska() {
-    cout << " kenijska ";
 }
--- nagl.h
+++
@@ -1,7 +0,0 @@
-extern int ile_pigmejow;
-extern int ile_europejczykow;
-
-void funkcja_etiopska();
-void funkcja_kenijska();
-void funkcja_francuska();
-void funkcja_niemiecka();
