2299261f99e1dd7d2d64f1670d851cec3cf8f859
mateuszbaszaraba <mateuszbaszaraba@outlook.com>
2020-12-12 20:49:00 +0100

    recursion

--- main.cpp
+++ main.cpp
@@ -2,50 +2,67 @@
 // Created by Mateusz Baszaraba on 08/12/2020.
 //
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int globalny_jedynkowy {25};
-int globalny_inny {77};
-
-int f_wartosc(int ktory)
-{
-    int lokalny {4};
-    if(ktory == 1) return globalny_jedynkowy;
-    else return globalny_inny;
-}
-
-int & f_ref_lwartosc(int ktory)
-{
-    int lokalny {4};
-    if(ktory == 1) return globalny_jedynkowy;
-    else return globalny_inny;
-}
-
-void wypisz(string txt, int a)
-{
-    cout << txt;
-    if(a) cout << a << ", ";
-    cout << "globalny_jedynkowy = " << globalny_jedynkowy << "globalny_inny = " << globalny_inny << endl;
-}
+int sumator(int jeszcze_krokow, int suma_dotychczas);
+void schodki(int ile, char znak);
+void dwojkowo(int liczba);
 
 int main() {
-    int n {0};
-    n = f_wartosc(1);
-    wypisz("rezultat= ", n);
-    n = f_wartosc(6);
-    wypisz("rezultat= ", n);
+    cout << "sumowanie liczb od 0 do n. Podaj wartość n: ";
+    int n;
+    cin >> n;
+    cout << "Suma liczb naturalinych od 0 do " << n << " to = \n" << sumator(n, 0) << endl;
 
-    n = f_ref_lwartosc(1);
-    wypisz("rezultat2= ", n);
-    n = f_ref_lwartosc(6);
-    wypisz("rezultat2= ", n);
 
-    f_ref_lwartosc(1) = 5000;
-    wypisz("wypisane", 0);
+    int liczba = 241;
+    cout << liczba << " to dwojkowo ";
+    dwojkowo(liczba);
+    cout << endl;
 
-    f_ref_lwartosc(6) = 7500;
-    wypisz("wypisane", 0);
     return 0;
+}
+
+int sumator(int jeszcze_krokow, int suma_dotychczas)
+{
+    int rezultat = 0;
+    static int krok_rekurencji;
+
+    int to_pietro_nr = krok_rekurencji;
+    krok_rekurencji++;
+    schodki(to_pietro_nr, '>');
+
+    cout << suma_dotychczas << " + " << to_pietro_nr << " = " << (suma_dotychczas + to_pietro_nr) << endl;
+    suma_dotychczas += to_pietro_nr;
+
+    if(jeszcze_krokow > 0) {
+        rezultat = sumator(jeszcze_krokow-1, suma_dotychczas);
+    } else {
+        cout << "...wracamy..." << endl;
+        rezultat = suma_dotychczas;
+    }
+    schodki(to_pietro_nr, '<');
+    cout << endl;
+    krok_rekurencji--;
+    return rezultat;
+}
+
+void schodki(int ile, char znak) {
+    cout << ile << " pietro: ";
+    for(int m=0; m<ile; m++)
+    {
+        cout << znak << " ";
+    }
+    cout << " ";
+}
+
+void dwojkowo(int liczba)
+{
+    int reszta = liczba%2;
+    if(liczba > 1)
+    {
+        dwojkowo(liczba/2);
+    }
+    cout << reszta;
 }
