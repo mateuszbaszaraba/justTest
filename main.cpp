bc757103e41c7382cbf58df0f695808d6ac0da63
mateuszbaszaraba <mateuszbaszaraba@outlook.com>
2020-12-16 16:21:32 +0100

    array to funct

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 //
 // Created by Mateusz Baszaraba on 08/12/2020.
 //
-#include <iostream>
 
-#define DDZ(color, data) int player_##color##_##data
+#include <iostream>
 
 using namespace std;
 
+void multiply(int howMany, long t[]);
 int main() {
+    constexpr int size {8192};
+    long vidmo[size];
 
-    DDZ(green, height) {40};  // int player_green_height {40}
+    for(int i=0; i<size; i++)
+    {
+        vidmo[i] = i;
+        if(i<6)
+            cout << "vidmo[" << i << "]= " << vidmo[i] << endl;
+    }
 
-    cout << player_green_height;
+    multiply(size, vidmo+3);
+    cout << "After multiply \n";
+    for(int i = 0; i < 9; i++)
+    {
+        cout << "vidmo[" << i << "]= " << vidmo[i] << endl;
+    }
 
     return 0;
+}
+
+void multiply(int howMany, long t[])
+{
+    for(int i = 0; i < howMany; i++)
+        t[i] *= 2;
 }
