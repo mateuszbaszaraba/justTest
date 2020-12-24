d3d5e832fc87ae415b16ad6b7d37e7ce5c962708
mateuszbaszaraba <mateuszbaszaraba@outlook.com>
2020-12-24 12:39:51 +0100

    char arr

--- main.cpp
+++ main.cpp
@@ -3,50 +3,24 @@
 //
 
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-enum class Tetap {
-    nic, scenopis, proba_czytana, proba_kamerowa, nagranie, montaz, postsynchron
-};
-
-void stan_realizacji(Tetap s[], int ile, int dzien);
 int main() {
-    const int liczba_scen = 10;
-    Tetap scena[liczba_scen] = {Tetap::scenopis, Tetap::proba_czytana, Tetap::proba_kamerowa};
 
-    int dzien_realizacji = 1;
+    char tekst[] = { "what do you want to do" };
+    char komunikat[120];
 
-    scena[2] = Tetap::nagranie;
-    scena[5] = Tetap::montaz;
-    scena[9] = Tetap::nagranie;
-    scena[4] = Tetap::proba_kamerowa;
-    stan_realizacji(scena, liczba_scen, dzien_realizacji);
+    strcpy(komunikat, tekst);
+    cout << komunikat << endl;
 
-    dzien_realizacji++;
+    strncpy(komunikat, "123456789128j0d18", 9);
+    cout << komunikat << endl;
 
-    using Tetapp = Tetap; //nowy alians, zmiana nazwy
-
-    scena[1] = Tetapp::proba_kamerowa;
-    scena[5] = Tetapp::postsynchron;
-    scena[0] = Tetapp::scenopis;
-    scena[8] = Tetapp::scenopis;
-    stan_realizacji(scena, liczba_scen, dzien_realizacji);
+    strcpy(komunikat, "nothinggg");
+    cout << komunikat << endl;
 
     return 0;
 }
 
-void stan_realizacji(Tetap s[], int ile, int dzien)
-{
-    cout << dzien << ". dzien realizacji filmu reklamowego\n" << "Stan realizacji: \n";
-    for(int i = 0; i < ile; i++)
-    {
-        cout << "Scena nr " << i << ", ";
-        int zaawansowanie = static_cast<int> (s[i]);  //rzutowanie
-        for (int j = 0; j < zaawansowanie; j++)
-        {
-            cout << "#";
-        }
-        cout << endl;
-    }
-}
