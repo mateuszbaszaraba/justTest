a975f54886ac44424d5d3ebf213bfa66752c419b
mateuszbaszaraba <mateuszbaszaraba@outlook.com>
2020-12-27 22:18:20 +0100

    pointers

--- main.cpp
+++ main.cpp
@@ -3,69 +3,20 @@
 //
 
 #include <iostream>
-#include <string>
-#include <iomanip>
-#include <vector>
 
 using namespace std;
 
-void changeVec(vector<vector<int>> &v);
-void disVec(vector<vector<int>> v, string info = "worksheet");
 int main() {
+    int num {32};
+    int *pointer = &num;
 
-    vector<vector<int>> worksheet
-            {
-                    { 10, 20, 30 },
-                    { 40, 1250, 260, 50 }
-            };
+    cout << *pointer << endl;
 
-    disVec(worksheet);
-    changeVec(worksheet);
-    disVec(worksheet);
-    worksheet.resize(1);
-    disVec(worksheet);
-    worksheet.resize(6);
-    for(int i=1; i < 7; ++i)
-    {
-        worksheet[i].resize(3);
-    }
-    disVec(worksheet);
-    worksheet[1].push_back(420);
-    worksheet.push_back({12, 42, 51});
-    disVec(worksheet);
-    worksheet[0].pop_back();
-    worksheet.pop_back();
-    disVec(worksheet);
+    num = 45;
+    cout << num << " " << *pointer << endl;
+
+    *pointer = 1;
+    cout << num << " " << *pointer << endl;
 
     return 0;
-}
-
-void disVec(vector<vector<int>> v, string info)
-{
-    cout << info << " amount of rows: " << v.size() << endl;
-
-    for(int i=0; i<v.size(); ++i)
-    {
-        cout << "Row id: " << i << " length: " << v[i].size() << " -----> |";
-        for(int j=0; j<v[i].size(); ++j)
-        {
-            cout << setw(4) << v[i][j] << " | ";
-        }
-        cout << endl;
-    }
-
-}
-
-void changeVec(vector<vector<int>> &v)
-{
-    cout << "changing..." << endl;
-
-    for(auto & el : v)
-    {
-        for(auto & ell : el)
-        {
-            ell += 100;
-        }
-    }
-    disVec(v, "changing");
 }
