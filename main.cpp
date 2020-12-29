0871d8db8076cac846837e2819bd102035f53a18
mateuszbaszaraba <mateuszbaszaraba@outlook.com>
2020-12-29 23:08:46 +0100

    array and pointer

--- main.cpp
+++ main.cpp
@@ -3,26 +3,54 @@
 //
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-    int arri[6];
-    double arrd[6];
 
     int *pi;
     double *pd;
 
-    pi = &arri[0]; // pi = arri;
-    pd = arrd;
+    int arrint[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    double arrdbl[10];
 
-    for(int i=0; i<6; ++i, ++pi, ++pd)
+    pd = &arrdbl[0];
+
+    for(int i=0; i<10; ++i)
     {
-        cout << "i = " << i
-        << ") pi = "
-        << reinterpret_cast<unsigned long>(pi)
-        << ", pd = "
-        << reinterpret_cast<unsigned long>(pd) << endl;
+        *(pd++) = i/10.0;
+    }
+
+    cout << "Start: " << endl;
+
+    pi = arrint;
+    pd = arrdbl;
+
+    for(int i=0; i<10; ++i)
+    {
+        cout << i << ") \t" << *pi << "\t\t" << *pd << endl;
+        pd++;
+        pi++;
+    }
+
+    pi = &arrint[4];
+    pd = arrdbl + 2;
+
+    for(int i=0; i<3; ++i)
+    {
+        *(pi++) = -222;
+        *(pd++) = 333.3;
+    }
+
+    cout << "Afterlife:" << endl;
+
+    pi = arrint;
+    pd = arrdbl;
+
+    for(int i=0; i<10; ++i)
+    {
+        cout << "arrint[" << i << "] = " << setw(4) << *(pi++) << "\t\tarrdbl[" << i << "] = " << setw(5) << *(pd++) << endl;
     }
 
     return 0;
