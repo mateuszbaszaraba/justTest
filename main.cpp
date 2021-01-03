8c9c2870fb7eb27a3b87e0bf9c4ecdd43aa6c3d2
mateuszbaszaraba <mateuszbaszaraba@outlook.com>
2021-01-03 15:17:06 +0100

    const pointer and const object

--- main.cpp
+++ main.cpp
@@ -8,32 +8,42 @@ using namespace std;
 
 int main() {
 
-    int size;
+    int a {5};
+    int b {100};
 
-    cout << "Size: ";
-    cin >> size;
+    int *pa = &a;                           //normal pointer
+    int *const const_p = &a;                //constant pointer
 
-    auto *arr = new int[size][5];   // int (*arr)[5] = new int[size][5]
+    //*const_p++;                           //cannot do that
+    *pa = 1;
+    *const_p = 2;
 
-    arr[1][3] = 12;
+    pa = &b;
+    // const_p = &b;                        // ERROR
 
-    for(int i=0; i<size; ++i)
-    {
-        for(int j=0; j<5; ++j)
-        {
-            arr[i][j] = (i*2)+j;
-        }
-    }
+    int arr[4] = {0, 1, 2, 3};
+    int tmp;
 
-    for(int i=0; i<size; ++i)
-    {
-        for(int j=0; j<5; ++j)
-        {
-            //cout << "arr[" << i << "][" << j << "] = " << arr[i][j] << endl;
-            cout << arr[i][j] << "\t";
-        }
-        cout << endl;
-    }
+    int *w;
+    const int *const_p2;
+    w=arr;
+    const_p2=arr;
+
+    tmp = *w;
+    tmp = *const_p2;
+
+    w++;
+    const_p2++;
+
+    *w = 4;
+    //*const_p2 = 5;                        //cannot do that
+
+    cout << *w << "wtf" << *const_p2 << endl;
+
+    int tmp2 = 4;
+
+    const int *const p3 = &tmp2;            //cannot 'move' and change value
+
 
     return 0;
 }
